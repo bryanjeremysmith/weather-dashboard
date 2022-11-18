07e1ed1061e935b2d5651774e108d08421b4cce6
BJ Smith <bryanjeremysmith@gmail.com>
2022-11-18 09:29:31 -0800

    added sample image from assignment and added sample code from the open weather api documentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 var APIKey = "9c53ae9c6aa68485786246f0493f1097";
+
+var city = "Portland";
+
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+fetch(queryURL)
